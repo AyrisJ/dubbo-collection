12471cd8972e8dfa0c2b626b432d87442a910aa1
yangjie <ayris@bogon.yaic.com.cn>
2021-09-13 19:27:30 +0800

    first commit

---
+++ dubbo-springboot-starter/DubboProviderDemo/src/main/java/com/ayris/dubbo/service/impl/DefaultDemoService.java
@@ -0,0 +1,17 @@
+package com.ayris.dubbo.service.impl;
+
+
+import com.ayris.dubbo.service.DemoService;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.factory.annotation.Value;
+
+@DubboService(version = "1.0.0")
+public class DefaultDemoService implements DemoService {
+
+    @Value("${dubbo.application.name}")
+    private String serviceName;
+
+    public String sayHello(String name) {
+        return String.format("[%s] : Hello, %s", serviceName, name);
+    }
+}
