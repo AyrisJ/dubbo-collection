aadd9de8bd21c82927f085421fa19be33cad2404
yangjie <jie.yangji@tongdun.net>
2021-12-31 17:52:41 +0800

    add dubbo auto start

---
+++ dubbo-starter-autoconfig/src/main/java/com/ayris/dubbo/impl/DemoServiceImpl.java
@@ -0,0 +1,11 @@
+package com.ayris.dubbo.impl;
+
+import com.ayris.dubbo.service.DemoService;
+
+public class DemoServiceImpl implements DemoService {
+
+    @Override
+    public String sayHello(String name) {
+        return null;
+    }
+}
---
+++ dubbo-starter-autoconfig/src/main/java/com/ayris/dubbo/service/DemoService.java
@@ -0,0 +1,5 @@
+package com.ayris.dubbo.service;
+
+public interface DemoService {
+    String sayHello(String name);
+}
